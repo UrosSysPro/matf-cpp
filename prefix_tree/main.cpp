6d49c3ad875270d16e8c9b2dbc0228dae18b84d5
Uros Karaleic <karaleicuu@gmail.com>
2025-10-05 16:53:40 +0200

    started prefix tree

--- prefix_tree/main.cpp
+++ prefix_tree/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<unordered_map>
+
+class Node;
+class Tree;
+
+class Tree {
+private:
+	Node *root;
+	Node* addWordImpl() {
+		return nullptr;
+	}
+	Node* removeWordImpl() {
+		return nullptr;
+	}
+	Node* containsWordImpl() {
+		return nullptr;
+	}
+public:
+	Tree(){}
+	void addWord(std::string word) {
+		root = addWordImpl();
+	}
+	void removeWord(std::string word){}
+	void containsWord(std::string word){}
+	~Tree(){}
+};
+
+class Node {
+public:
+	bool isWord;
+	std::unordered_map<char,Node> children;
+	Node() {
+		isWord = false;
+		children=std::unordered_map<char,Node>();
+	}
+};
 
 int main() {
-	std::cout<<"hello";
+	Tree t=Tree();
+
     return 0;
 }
