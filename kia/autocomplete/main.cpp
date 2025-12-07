f1360efd5a987bfa19f393ec8280f132f8803d1a
Uros Karaleic <karaleicuu@gmail.com>
2025-12-07 13:26:53 +0100

    cas 1

--- kia/autocomplete/main.cpp
+++ kia/autocomplete/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
-#include <cstdio>
+#include <string>
+#include <vector>
+#include <map>
 
 int main(int argc, char** argv) {
-    for (int i=0; i<argc ;i++) {
-        std::printf("%s\n",argv[i]);
+
+    int n, q;
+    std::cin >> n >> q;
+
+    std::map<std::string, std::vector<std::string>> autocomplete;
+
+    for (int i = 0; i < n; i++) {
+        std::string word;
+        std::cin >> word;
+
+        for (int i = 0; i <= word.size(); i++) {
+            autocomplete[word.substr(0, i)].push_back(word);
+        }
     }
+
+    for (int i = 0; i < q; i++) {
+        std::string query;
+        std::cin >> query;
+
+        for (auto word : autocomplete[query]) {
+            std::cout << word << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
