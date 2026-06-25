832639e57cf3fc578e11d6086af5bbfdb5c820b8
Uros Karaleic <karaleicuu@gmail.com>
2026-06-25 23:14:43 +0200

    prefix tree

--- kia/data_structures/prefix_tree/main.cpp
+++ kia/data_structures/prefix_tree/main.cpp
@@ -5,18 +5,80 @@
 using namespace std;
 
 class PrefixTree;
+class Node;
+
+struct Node {
+    bool isWord{};
+    unordered_map<char, Node*> children;
+    static Node* empty() {
+        auto node = new Node();
+        node->isWord = false;
+        node->children = unordered_map<char, Node*>();
+        return node;
+    }
+};
 
 class PrefixTree {
+private:
+    Node *root = Node::empty();
+    Node* addR(Node* node, string word) {
+        if (node == nullptr) node = new Node();
+
+        if (word.empty()) {
+            node->isWord = true;
+        }else {
+            node->children[word[0]] = addR(node->children[word[0]], word.substr(1));
+        }
+        return node;
+    }
+
+    void removeR(Node* node, string word) {
+        if (node == nullptr) return;
+
+        if (word.empty()) {
+            node->isWord = false;
+        }else {
+            removeR(node->children[word[0]], word.substr(1));
+        }
+    }
+
+    bool hasR(Node* node, const string& word) {
+        if (node == nullptr) return false;
+
+        if (word.empty() && node->isWord) return true;
+
+        if (word.empty()) return false;
+
+        return hasR(node->children[word[0]], word.substr(1));
+    }
+    bool printR(Node* node, const string& prefix) {
+        if (node == nullptr) return false;
+
+        if (word.empty() && node->isWord) return true;
+
+        if (word.empty()) return false;
+
+        return hasR(node->children[word[0]], word.substr(1));
+    }
+    bool deleteR(Node* root) {
+        for (auto& [key, value]: root->children) {
+            deleteR(value);
+        }
+        delete root;
+    }
 public:
     PrefixTree() {}
-    ~PrefixTree() {}
-    void add(string value){}
-    void remove(string value){}
-    bool has(string value){ return false; }
+    ~PrefixTree() { deleteR(root); }
+    void add(string value){ root = addR(root, value); }
+    void remove(string value){ removeR(root, value); }
+    bool has(string value){ return hasR(root, value); }
+    void print(){}
+
 };
 
 int main(int argc, char *argv[]) {
     PrefixTree tree = PrefixTree();
+
     vector<string> addedWords = vector<string>();
     vector<string> removedWords = vector<string>();
     vector<string> testWords = vector<string>();
@@ -29,12 +91,23 @@ int main(int argc, char *argv[]) {
     removedWords.push_back("hello");
     removedWords.push_back("hello hello");
 
-    addedWords.push_back("hello");
-    addedWords.push_back("hello world");
-    addedWords.push_back("hello hello");
-    addedWords.push_back("hello2");
+    testWords.push_back("hello");
+    testWords.push_back("hello world");
+    testWords.push_back("hello hello");
+    testWords.push_back("hello2");
 
-    cout<<"contai";
+    for (auto& word : addedWords) {
+        tree.add(word);
+    }
+
+    for (auto& word : removedWords) {
+        tree.remove(word);
+    }
+
+    for (auto& word : testWords) {
+        bool contains = tree.has(word);
+        cout << "contains " << word << " " << contains << endl;
+    }
 
     return 0;
 }
