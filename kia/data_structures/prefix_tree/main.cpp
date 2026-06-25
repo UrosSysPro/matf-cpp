35e1085669ad0cdd92b2531557eefac88922450e
Uros Karaleic <karaleicuu@gmail.com>
2026-06-25 21:55:03 +0200

    dijkstra i prefix tree pocetak

--- kia/data_structures/prefix_tree/main.cpp
+++ kia/data_structures/prefix_tree/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
+class PrefixTree;
+
+class PrefixTree {
+public:
+    PrefixTree() {}
+    ~PrefixTree() {}
+    void add(string value){}
+    void remove(string value){}
+    bool has(string value){ return false; }
+};
+
 int main(int argc, char *argv[]) {
-    std::cout << "hello";
+    PrefixTree tree = PrefixTree();
+    vector<string> addedWords = vector<string>();
+    vector<string> removedWords = vector<string>();
+    vector<string> testWords = vector<string>();
+
+    addedWords.push_back("hello");
+    addedWords.push_back("hello world");
+    addedWords.push_back("hello hello");
+    addedWords.push_back("hello2");
+
+    removedWords.push_back("hello");
+    removedWords.push_back("hello hello");
+
+    addedWords.push_back("hello");
+    addedWords.push_back("hello world");
+    addedWords.push_back("hello hello");
+    addedWords.push_back("hello2");
+
+    cout<<"contai";
+
+    return 0;
 }
